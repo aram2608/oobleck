36a7d77a1573eb08cc58639c1113b7717d8845f9
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-12-29 21:24:55 -0700

    sdl is a massive pain

--- oobleck/src/main.c
+++ oobleck/src/main.c
@@ -21,7 +21,10 @@ void initializeSDL(void) {
     }
 }
 
-void closeSDL(SDL_Window* window) {
+void closeSDL(SDL_Window* window, SDL_Texture* text, SDL_Renderer* renderer, TTF_Font* font) {
+    TTF_CloseFont(font);
+    SDL_DestroyTexture(text);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     TTF_Quit();
     SDL_Quit();
@@ -37,13 +40,37 @@ int main(int argc, char** argv) {
     printf("Buffer: %s\n", buff->data);
 
     initializeSDL();
+    TTF_Font* font = TTF_OpenFont("./font/Fira_Code/static/FiraCode-Light.ttf", 10);
+    SDL_Color color = {
+        .r = 255,
+        .g = 255,
+        .b = 255,
+        .a = 255,
+    };
+
+    if (font == NULL) {
+        printf("PANIC: error loading font\n");
+        exit(1);
+    }
+
+    SDL_Surface* textSurface = TTF_RenderText_Blended(font, "Text", 0, color);
+
+    if (textSurface == NULL) {
+        printf("PANIC: failure creating text surface");
+        exit(1);
+    }
 
     SDL_Window* window = SDL_CreateWindow("oobleck", 600, 600, SDL_WINDOW_RESIZABLE);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+
+    SDL_Texture* textImage = SDL_CreateTextureFromSurface(renderer, textSurface);
+    SDL_DestroySurface(textSurface);
 
     bool run = true;
     SDL_Event event;
 
     while(run) {
+        SDL_RenderTexture(renderer, textImage, NULL, NULL);
         while(SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
                 run = false;
@@ -53,6 +80,6 @@ int main(int argc, char** argv) {
 
     umkaFree(umka);
     destroyBuffer(buff);
-    closeSDL(window);
+    closeSDL(window, textImage, renderer, font);
     return 0;
 }
